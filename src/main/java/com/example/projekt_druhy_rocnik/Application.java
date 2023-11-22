3a4aa9064612455cafbf8bd299dc1b95a6220d52
Projekt_druhy_rocnik <jan.kalisz007@gmail.com>
2023-11-22 13:10:06 +0100

    Vytvoření 3 buttonů přičemž jeden vypisuje text

--- src/main/java/com/example/projekt_druhy_rocnik/Application.java
+++ src/main/java/com/example/projekt_druhy_rocnik/Application.java
@@ -5,11 +5,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("document-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Primitivní účetní evidence");
         stage.setScene(scene);
--- src/main/java/com/example/projekt_druhy_rocnik/DocumentController.java
+++ src/main/java/com/example/projekt_druhy_rocnik/DocumentController.java
@@ -1,5 +1,14 @@
 package com.example.projekt_druhy_rocnik;
 
-public class DocumentController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
+public class DocumentController {
+    @FXML
+    private Label test;
+
+    @FXML
+    protected void onDoklad1ClickButton(){
+        test.setText("testuju jestli funguje");
+    }
 }
